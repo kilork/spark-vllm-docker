4967d23290e8d280ab9763f10f3af5ec0becfdc6
Alexander Korolev <alexander.korolev.germany@gmail.com>
2026-03-15 23:35:54 +0100

    feat(recipes): Intel/MiniMax-M2.5-int4-AutoRound with tokenizer mod

---
+++ mods/minimax-m2.5/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+MODEL_NAME=${MODEL_NAME:-Intel/MiniMax-M2.5-int4-AutoRound}
+MODEL_HUB=${MODEL_NAME/\//"--"}
+MODEL_PREFIX=/root/.cache/huggingface/hub/models--${MODEL_HUB}
+MODEL_PATH=$MODEL_PREFIX/snapshots/$($MODEL_PREFIX/refs/main)
+
+TOKENIZER=/minimax-m2.5-tokenizer
+
+mkdir $TOKENIZER
+cd $TOKENIZER
+cp $MODEL_PATH/{chat_template.jinja,tokenizer.json,tokenizer_config.json}
+sed -i 's/"tokenizer_class": *"[^"]*"/"tokenizer_class": "TokenizersBackend"/' tokenizer_config.json
+tail -n5 tokenizer_config.json
